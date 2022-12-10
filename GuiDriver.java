5f21b7b946dc2c3ae4ec7c2b345118c2ae1c4c47
jacyong <90311432+kurochiii@users.noreply.github.com>
2022-12-10 18:27:34 +0800

    Update GuiDriver.java

--- GuiDriver.java
+++ GuiDriver.java
@@ -7,29 +7,26 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.FlowLayout;
 import javax.swing.JOptionPane;
+import javax.swing.SwingWorker;
 
-public class GuiDriver {
-    
-    /** 
-     * The main method allows the program to run the GUI or in other words display the program
-     * @param args is a String array needed to run the program
-     */
+public class GuiDriver{ 
     public static void main(String[] args) {
 
         JFrame sFrame = new JFrame();
         sFrame.setTitle("Start Game");
         sFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        sFrame.setSize(500, 500);
-        sFrame.setResizable(false);
-        sFrame.setLayout(new FlowLayout());
+        sFrame.setSize(300, 200);
+        sFrame.setResizable(false); 
+        sFrame.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 40));
+        sFrame.getContentPane().setBackground(new Color(0x36454F));
 
-        ImageIcon newgame = new ImageIcon("shovel.png");
-        ImageIcon exitgame = new ImageIcon("potato.png");
+        ImageIcon newgame = new ImageIcon("Start.png");
+        ImageIcon exitgame = new ImageIcon("Exit.png");
         ImageIcon tryagain = new ImageIcon("mango.png");
 
         JButton startBtn = new JButton();
         startBtn.setIcon(newgame);
-        startBtn.setBackground(new Color(0xE3D308));
+        startBtn.setBackground(new Color(0x36454F));
         startBtn.setContentAreaFilled(true);
         startBtn.addActionListener(new ActionListener(){
             @Override
@@ -56,26 +53,12 @@ public class GuiDriver {
                 new lotClass(frame1, myfarm, SelectedLot);
 
                 frame1.setVisible(true); 
-
-                Boolean endGame = false;
-                
-                while(endGame == false)
-                {
-                    //JOptionPane.showMessageDialog(null, "Check", null, JOptionPane.INFORMATION_MESSAGE);
-                    if(myfarm.checkEndGame() == true)
-                    {
-                        JOptionPane.showMessageDialog(null, "End Game", null, JOptionPane.INFORMATION_MESSAGE);
-                        sFrame.setTitle("End Game");
-                        sFrame.setVisible(true);
-                        endGame = true;
-                    }
-                }
             }
         });
 
         JButton exitBtn = new JButton();
         exitBtn.setIcon(exitgame);
-        exitBtn.setBackground(new Color(0xE3D308));
+        exitBtn.setBackground(new Color(0x36454F));
         exitBtn.setContentAreaFilled(true);
         exitBtn.addActionListener(new ActionListener(){
             @Override
@@ -89,23 +72,5 @@ public class GuiDriver {
         sFrame.add(exitBtn);
 
         sFrame.setVisible(true);
-
     }  
-
-    public static void NewFarm(JFrame frame1, MyFarm myfarm, Farmer myfarmer)
-    {
-        frame1.setTitle("My Farming Adventures");
-        frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame1.setSize(1050, 700);
-        frame1.setResizable(false);
-        frame1.setLayout(new BorderLayout());
-    
-        myfarm.setGame();
-        int [] SelectedLot = new int[2];
-
-        new Welcome(frame1, myfarm, myfarmer);
-        new toolClass(frame1, myfarm, myfarmer, SelectedLot);
-        new seedClass(frame1, myfarm, myfarmer, SelectedLot);
-        new lotClass(frame1, myfarm, SelectedLot);
-    }
 }
